c87018469a6812b87ef719d6b3dd8f157f9922c5
solidSpoon <cedarrival@outlook.com>
2024-05-12 12:53:06 +0800

    文案

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -7,13 +7,13 @@ const links = [
     {
         name: 'Blogs',
         description:
-            'Writing on software design, company building, and the aerospace industry.',
+            'I write to share my thoughts. I hope you enjoy reading them as much as I enjoy writing them.',
         link: {href: 'http://solidspoon.xyz/blog', label: 'solidspoon.xyz/blog'},
     },
     {
         name: 'Notes',
         description:
-            'Short-form thoughts on programming, leadership, product design, and more.',
+            'Short notes on programming, product design, and more. I use this to keep track of my thoughts.',
         link: {href: 'http://solidspoon.xyz/note', label: 'solidspoon.xyz/note'},
     },
 ]
@@ -30,7 +30,7 @@ export default async function Articles() {
                         <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
                             <Card.Link href={project.link.href}>{project.name}</Card.Link>
                         </h2>
-                        {/*<Card.Description>{project.description}</Card.Description>*/}
+                        <Card.Description>{project.description}</Card.Description>
                         <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
                             <LinkIcon className="h-6 w-6 flex-none"/>
                             <span className="ml-2">{project.link.label}</span>
